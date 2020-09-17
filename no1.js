a741700b903aaa092aebe0dd5c85518e20ed328a
Dzulfanni <belajarayok866@gmail.com>
2020-09-17 09:43:54 +0700

    first commit

---
+++ no1.js
@@ -0,0 +1,4 @@
+var listDesa = ['argodadi,sedayu','argomulyo,sedayu','argorejo,sedayu','argosari','bangunjiwo,kasihan','banguntapan,banguntapan','bantul,bantul','baturetno,banguntapan','bawuran,pleret','canden,jetis','caturharjo,pandak','cetan srigandi'];
+for(i in listDesa){
+    document.write('<li>'+listDesa[i]+'</li>');
+}
